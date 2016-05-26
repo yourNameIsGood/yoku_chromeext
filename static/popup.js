a012d1d445e2e946f7d4fae180bffc7bdbfeb2a8
jack20039 <jack20039@hotmail.com>
2016-05-26 14:12:26 +0800

    add debug arg

--- static/popup.js
+++ static/popup.js
@@ -6,38 +6,49 @@ chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}
 
       // chrome.tabs.executeScript(tabs.id, {code: 'hh();', allFrames: true});
 
+      //点击decode button触发解析
       $(".decode-btn").on("click",function(){
-      	  var current_url = (tabs[0].url);
-	      var input_url = $(".input-url").val();
-	      var decode_url = input_url!=""?input_url:current_url;
-	      var res = decodeId(decode_url,"U");
-	      $(".result").html(res);
+              var current_url = (tabs[0].url);
+            var input_url = $(".input-url").val();
+            var decode_url = input_url!=""?input_url:current_url;
+            var res = decodeId(decode_url,"U");
+            $(".result").html(res);
+      });
 
+      //点击debug button 在url最后追加debug参数
+      $(".debug-btn").on("click",function(){
+            var current_url = (tabs[0].url);
+            console.log(current_url);
+            current_url += "&debug=f53bafd8c2b602abca59e4c2850d8";
+            console.log(current_url);
+            window.location.href = current_url;
+            window.open(current_url);
       });
 
       //测试数据：
       var list_data = [
-      		{'list_name':'边看边买','num_id':'177428936',"encode_id":'XNzA5NzE1NzQ0'},
-      		{'list_name':'(秋千)图文注释','num_id':'227047537',"encode_id":'XOTA4MTkwMTQ4'},
-      		{'list_name':'打赏','num_id':'207232552',"encode_id":'XODI4OTMwMjA4'},
-      		{'list_name':'投票','num_id':'207242262',"encode_id":'XODI4OTY5MDQ4'},
+                  {'list_name':'边看边买, 钟伟弹弓','num_id':'319477320',"encode_id":'XMTI3NzkwOTI4MA=='},
+                  {'list_name':'边看边买, 药瓶','num_id':'177428936',"encode_id":'XNzA5NzE1NzQ0'},
+                  {'list_name':'(秋千)图文注释','num_id':'227047537',"encode_id":'XOTA4MTkwMTQ4'},
+                  {'list_name':'打赏','num_id':'207232552',"encode_id":'XODI4OTMwMjA4'},
+                  {'list_name':'投票','num_id':'207242262',"encode_id":'XODI4OTY5MDQ4'},
       ];
 
       var http = "http://linzhen.youku.com";
       var str = "";
       for(var i in list_data){
-      	// console.log(list_data[i]);
-      	var d = list_data[i];
-	      	str+="<span class=\"list-name\">"+d.list_name+": </span>";
-	      	str+="<span class=\"num-id\">"+d.num_id+"</span>";
-	      	str+="<span class=\"encode-id\"> "+ d.encode_id+"</span><br>";
-	      	str+="<a href=\""+http+"/v/timelineH5forAppCard?videoId="+d.encode_id+"\" target=\"_blank\">CARD</a> ";
+            // console.log(list_data[i]);
+            var d = list_data[i];
+                  str+="<span class=\"list-name\">"+d.list_name+": </span>";
+                  str+="<span class=\"num-id\">"+d.num_id+"</span>";
+                  str+="<span class=\"encode-id\"> "+ d.encode_id+"</span><br>";
+                  str+="<a href=\""+http+"/v/timelineH5forAppCard?videoId="+d.encode_id+"\" target=\"_blank\">CARD</a> ";
                   str+="<a href=\""+http+"/v/timelineH5forAppV2?videoId="+d.encode_id+"&h5_type=2\" target=\"_blank\">V2</a> ";
-	      	str+="<a href=\""+http+"/interact/app/get/timeLinePlugin?data={%22platform%22:10,%22platform_type%22:102,%22device%22:1,%22Sc%22:1,%22vid%22:%22"+d.encode_id+"%22,%22showId%22:0,%22card_width%22:320}&callback=jsonp1"+"\" target=\"_blank\">timelinePlugin接口</a><br>";
+                  str+="<a href=\""+http+"/interact/app/get/timeLinePlugin?data={%22platform%22:10,%22platform_type%22:102,%22device%22:1,%22Sc%22:1,%22vid%22:%22"+d.encode_id+"%22,%22showId%22:0,%22card_width%22:320}&callback=jsonp1"+"\" target=\"_blank\">timelinePlugin接口</a><br>";
       }
       $(".video-list p").append(str);
-      
-      //添加常用uid：    //九位数的id在经历encodeId时会出错
+
+      //添加常用uid：
       var user_list = [
                   {'list_name':'海川','num_id':'464158176',"encode_id":'UMTg1NjYzMjcwNA=='},
                   {'list_name':'seiitsu720','num_id':'121998474',"encode_id":'XODI4OTMwMjA4'},
